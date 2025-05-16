8dea93103892fce11510b1601058e41683f00cd4
dreifusjack <dreifus.j@northeastern.edu>
2025-05-16 09:38:42 -0400

    Changed hero background to include image

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,12 +1,20 @@
 import CTAButton from "../ui/CTAButton";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section
-      id="home"
-      className="h-250 bg-[linear-gradient(to_top,#fcf6d7_1%,#e2dab3_25%,#709be5_60%)]"
-    >
-      <div className="h-200 flex flex-col justify-center items-center space-y-6 px-4 text-center">
+    <section id="home" className="h-250 relative overflow-hidden">
+      <div className="absolute inset-0 z-0">
+        <Image
+          src="/hero_background.png"
+          alt="Lighthouse on cliff"
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+        />
+      </div>
+      <div className="absolute inset-0 z-10 bg-gradient-to-b from-transparent via-transparent to-[#fcf6d7] opacity-100"></div>
+      <div className="relative z-20 h-200 flex flex-col justify-center items-center space-y-6 px-4 text-center">
         <div className="max-w-2xl">
           <h1 className="text-[color:var(--light)] text-[48px]">
             We Buy & Build Software Tools For Entreprenuers
