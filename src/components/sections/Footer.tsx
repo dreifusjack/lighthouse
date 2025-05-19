1390ca70a7b4f456815ebb4594370c7ebe093794
dreifusjack <dreifus.j@northeastern.edu>
2025-05-19 11:16:24 -0400

    footer in

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,3 +1,52 @@
+"use client";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import Image from "next/image";
+
 export default function Footer() {
-  return <div style={{ height: "250" }}>footer</div>;
+  const handleLinkedInClick = () => {
+    window.open(
+      "https://www.linkedin.com/company/lighthouse-search-fund/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  const handlePersonalLinkedInClick = () => {
+    window.open(
+      "https://www.linkedin.com/in/jack-dreifus/",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+
+  return (
+    <div className="h-[250px] pt-[75px]">
+      <div className="ms-[10%] max-w-[80%] flex flex-col">
+        <div className="flex flex-row justify-between items-end mb-4">
+          <Image src="/lighthouse_logo.png" alt="" width={50} height={50} />
+          <div
+            onClick={handleLinkedInClick}
+            className="cursor-pointer hover:text-[var(--blue)] transition-colors"
+          >
+            <LinkedInIcon fontSize="large" />
+          </div>
+        </div>
+        <hr className="border-t border-[var(--charcoal)] w-full" />
+        <div className="flex flex-row items-center mt-4 self-center">
+          <CopyrightIcon fontSize="small" />
+          <p className="ml-1">2025 Lighthouse. All Rights Reserved.</p>
+        </div>
+        <p className="text-center mt-2">
+          Design & Development:{" "}
+          <span
+            className="underline cursor-pointer hover:text-[var(--blue)] transition-colors"
+            onClick={handlePersonalLinkedInClick}
+          >
+            Jack Dreifus
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
